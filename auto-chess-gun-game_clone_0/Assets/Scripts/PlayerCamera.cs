6ad7c2234058f3f74f9df864d021d97600e9c874
AtlasWongy <70804117+AtlasWongy@users.noreply.github.com>
2023-01-27 22:55:02 +0800

    client-server-update

---
+++ auto-chess-gun-game_clone_0/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float mouesX = Input.GetAxis("Mouse X") * Time.deltaTime * sensX;
+        float mouesY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouesX;
+        xRotation -= mouesY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
